d9833b4191b15dbe24ce93973ab461a886cb954d
0lch4 <127250399+0lch4@users.noreply.github.com>
2023-04-18 17:00:14 +0200

    wyszukiwanie tytulow

---
+++ api.py
@@ -0,0 +1,34 @@
+import requests
+import json
+import html
+import os
+
+api_key = os.environ.get('YouTube_API_KEY')
+
+url = 'https://www.googleapis.com/youtube/v3/search'
+
+tytul = input("podaj nazwe piosenki")
+params = {
+    'q': tytul,
+    'key': api_key,
+    'type': 'song',
+    'part': 'id,snippet'
+}
+
+response = requests.get(url, params=params)
+
+if response.status_code == 200:
+    data = response.json()
+    with open('wynik.json', 'w', encoding='utf-8') as f:
+        wynik = json.dump(data, f, indent=2, ensure_ascii=False)
+    with open('wynik.json', 'r', encoding='utf-8') as f:
+        plik = f.read()
+        for line in plik.splitlines():
+            if line.strip().startswith('"title":'):
+                line = line.strip()[9:]
+                line = line.strip()[1:-2]
+                line = html.unescape(line)
+                print(line)
+                break
+else:
+    print(f'Błąd {response.status_code}: {response.reason}')

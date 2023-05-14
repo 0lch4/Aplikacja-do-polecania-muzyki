d9047af7080837f55f4189366b9a9e50578f193a
0lch4 <127250399+0lch4@users.noreply.github.com>
2023-05-14 17:11:59 +0200

    .

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-import subprocess
-#uruchamia wszystkie programy po sobie
+from fastapi import FastAPI, Form, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
-subprocess.call(["python", "song_analize.py"])
+#utworzenie aplikacji
+app = FastAPI()
 
-subprocess.call(["python", "AI.py"])
+#polaczenie z katalogiem z widokami
+templates = Jinja2Templates(directory="templates")
 
-subprocess.call(["python", "new_parameters.py"])
+#polaczenie ze stylami
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+#widok glowny ktory wyswietla sie pod adresem strony
+@app.get("/", response_class=HTMLResponse)
+async def show_form(request: Request):
+    return templates.TemplateResponse("main.html", {"request": request})
+
+@app.post("/submit", response_class=HTMLResponse)
+async def submit_form(request: Request, songs: str = Form(...)):
+     return templates.TemplateResponse("genres.html", {"request": request, "songs": songs})
+        
---
+++ mainn.py
@@ -0,0 +1,8 @@
+import subprocess
+#uruchamia wszystkie programy po sobie
+
+subprocess.call(["python", "song_analize.py"])
+
+subprocess.call(["python", "AI.py"])
+
+subprocess.call(["python", "new_parameters.py"])
